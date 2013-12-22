ec34f4e2b6b6f192de729d1c577811bc7e069f62
Wray Bowling <fabmud@gmail.com>
2013-12-21 20:25:02 -0500

    Found out how to visualize different line segments

--- demos/spiro/spiro.js
+++ demos/spiro/spiro.js
@@ -363,13 +363,6 @@ function seg_to_bez(ctx, ks, x0, y0, x1, y1) {
 			var vl = scale3 * Math.sin(th_even - th_odd);
 			var ur = scale3 * Math.cos(th_even + th_odd);
 			var vr = scale3 * Math.sin(th_even + th_odd);
-			//BOOKMARK
-			if(alternating_stroke){
-				ctx.strokeStyle = "rgb(0, 255, 255)";
-			}else{
-				ctx.strokeStyle = "rgb(210, 0, 121)";
-			}
-			alternating_stroke = !alternating_stroke;
 			ctx.bezierCurveTo(x0 + ul, y0 + vl, x1 - ur, y1 - vr, x1, y1);
 		} else {
 			console.log('bend > 1 (subdivide)');
@@ -719,15 +712,30 @@ SpiroUi.prototype.paint = function() {
 			case 1: ctx.strokeStyle = "rgb(64, 0, 0)"; break;
 			case 2: ctx.strokeStyle = "rgb(255, 0, 0)"; break;
 		}
-		ctx.beginPath();
-		ctx.moveTo(nodes[0].xy[0], nodes[0].xy[1]);
+		
+		//wray removed
+		//ctx.beginPath();
+		//ctx.moveTo(nodes[0].xy[0], nodes[0].xy[1]);
 		var segs = spline.segs;
 		for (var i = 0; i < segs.length; i++) {
 			var seg = segs[i];
 			var ths = seg.get_ths();
 			var ks = fit_euler(ths[0], ths[1]).ks;
 			ks[2] = 0; ks[3] = 0;
+			//wray added
+			ctx.beginPath();
+			ctx.moveTo(nodes[i].xy[0], nodes[i].xy[1]);
+			//BOOKMARK
+			//ctx.beginPath(); // wray's : force line to draw as segments
+			if(alternating_stroke){
+				ctx.strokeStyle = "rgb(0, 255, 255)";
+			}else{
+				ctx.strokeStyle = "rgb(210, 0, 121)";
+			}
+			alternating_stroke = !alternating_stroke;
 			seg_to_bez(ctx, ks, seg.left.xy[0], seg.left.xy[1], seg.right.xy[0], seg.right.xy[1]);
+			ctx.stroke();
+			ctx.closePath();
 		}
 		ctx.stroke();
 	}
